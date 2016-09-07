405192eb5f20381607a454a6a58e3b873b0ce1ad
Nate Elliott <nelliott.ca@gmail.com>
2016-09-07 15:02:39 -0300

    gtg

--- playgound/app/__init__.py
+++ playgound/app/__init__.py
@@ -1,30 +1,7 @@
-# BS22NF
-from flask import Flask, jsonify, request, send_from_directory
-import os
-from .fake import FakeDB, FakeCred
+from flask import Flask
 
 app = Flask(__name__)
 
-
-app.config["IP"] = "127.0.0.1"
-app.config["HTTP_TYPE"] = "http"
-app.config["HTTP_PORT"] = "5000"
-
-app.config["UPLOAD_FOLDER"] = "upload\\"
-app.config["DATABASE_FOLDER"] = "\\db\\"
-
-app.config["APP_ROOT"] = os.path.dirname(os.path.abspath(__file__))
-app.config["PROJECT"] = "playground"
-app.config["VERSION"] = "1.0"
-
-app.config["TOKEN_EXPIRE"] = (60 * 60 * 24) # 24 HOURS
-
-app.config["API_ROOT"] = "/" + app.config["PROJECT"] + "/api/v" + app.config["VERSION"] + "/"
-
-app.config["URL_ROOT"] = app.config["HTTP_TYPE"] + "://" + \
-                         app.config["IP"] + ":" + \
-                         app.config["HTTP_PORT"]
-
-app.config["ABR_ROOT"] = "/@"
+app.config.from_object("config")
 
 import views
